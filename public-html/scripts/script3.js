3e3c02e7f8148f4c9560af9d865f9de93113d3b9
saulocaceresg <scaceresg@unsa.edu.pe>
2025-12-04 15:30:50 -0500

    comentarios en script3.js

---
+++ public-html/scripts/script3.js
@@ -0,0 +1,15 @@
+// 5.	Usa async/await para obtener altura y peso de Pikachu
+console.log("Ejercicio 3 (6.)\n\"Altura y peso de Pikachu (async/await)\"");
+
+async function pesoAlturaPikachu() {
+    console.log("'Pikachu'");
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`); // Se llama al fetch con await
+
+    const data = await response.json(); // Se convierte a json
+    
+    // Se imprimen datos
+    console.log("Altura:", data.height);
+    console.log("Peso:", data.weight);
+}
+
+pesoAlturaPikachu();

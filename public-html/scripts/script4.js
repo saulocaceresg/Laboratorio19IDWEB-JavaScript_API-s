9060d0cab97e9457f0b6d2ee817f4e01d32b1ca2
saulocaceresg <scaceresg@unsa.edu.pe>
2025-12-04 19:19:32 -0500

    Commit 4: cuarto ejercicio (5.) -> front default de charizard

--- public-html/scripts/script4.js
+++ public-html/scripts/script4.js
@@ -1,2 +1,9 @@
 // 6.	Muestra en consola la URL de sprites.front_default de Charizard
-console.log("Ejercicio 4 (6.)\n\"Altura y peso de Pikachu (async/await)\"");
+console.log("Ejercicio 4 (6.)\n\"front default de charizard (async/await)\"");
+
+fetch(`https://pokeapi.co/api/v2/pokemon/charizard`)
+    .then(response => response.json())
+    .then(data => {
+        console.log("front default:", data.sprites.front_default); // Se accede a la url
+    })
+    .catch(error => console.error("Error:", error));
